361af62ad431e3219b3aa89f6b3b59240e44daf7
xkortex <mikemcdermott23@gmail.com>
2017-09-07 13:44:12 -0400

    line chart props are passed to svg props...not entirely sure how , see https://github.com/recharts/recharts/issues/469

--- src/xile_chart.js
+++ src/xile_chart.js
@@ -6,13 +6,13 @@ import * as Recharts from 'recharts';
 
 const {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} = Recharts;
 var data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+      {name: 'Page A', uv: 0, foo: 1, pv: 2400, amt: 2400},
+      {name: 'Page B', uv: 0, foo: 1, pv: 1398, amt: 2210},
+      {name: 'Page C', uv: 0, foo: 1, pv: 9800, amt: 2290},
+      {name: 'Page D', uv: 2780, foo: 1, pv: 3908, amt: 2000},
+      {name: 'Page E', uv: 1890, foo: 1, pv: 4800, amt: 2181},
+      {name: 'Page F', uv: 2390, foo: 1, pv: 3800, amt: 2500},
+      {name: 'Page G', uv: 3490, foo: 1, pv: 4300, amt: 2100},
 ];
 var i =1000;
 const XileLineChart = React.createClass({
@@ -42,6 +42,8 @@ const XileLineChart = React.createClass({
        <Legend />
        <Line type="monotone" dataKey="pv" stroke="#8884d8" />
        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+       <Line type="monotone" dataKey="amt" stroke="#2299AA" strokeWidth={20} />
+
       </LineChart>
     );
   }
